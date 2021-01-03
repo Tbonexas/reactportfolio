ba671f6c0fb003f76f37b2cefdf3d385913f76a5
Todd Murdoch <TheClash090@gmail.com>
2021-01-03 15:27:00 -0800

    fixed small typos before deploying to gh pages.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,6 @@ function Footer() {
          <Container fluid={true}>
              <Row className="border-top justify-content-between p-3">
               <Col className="p-0" md={3} sm={12}>
-              GitHub
               </Col>
               <Col className="p-0 d-flex justify-content-end" md={3}>
                 Created by Todd M. Wayment.
